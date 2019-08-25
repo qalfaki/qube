ec1a0f7a192b9b9a70b7f0ac881d37f1c661e356
syntaxsizer <qusai.alfaki@gmail.com>
2019-08-25 16:30:56 +0300

    integrate reset password page to firebase api

--- src/components/reset.js
+++ src/components/reset.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import logo from '../assets/images/logo196x143.png';
+import { withRouter } from 'react-router-dom';
 import useCredintialsValidation from '../reducers/useCredintialsValidation.js';
+import { withFirebase } from '../api';
+import { compose } from 'recompose';
 
 const Reset = (props) =>{
 	
   const {email, setEmail, emailValid} = useCredintialsValidation();
   const [error, setError] = React.useState('')
+
+  const handleClick = ()=> {
+    if (emailValid) {
+      props.firebase.resetPassword(email).then(()=> {
+        console.log('the resetPassword was fired')
+        setError('A rest link was sent your email')
+        setTimeout(()=>{props.history.push('/login')}, 5000);
+      }).catch(error=>setError(error.message))
+    }
+  }
 
   return (
   	<div className="top-height col-12 row"><div className="mx-auto d-block">
@@ -16,11 +29,16 @@ const Reset = (props) =>{
     	<input className="input-field form-control d-block mx-auto" type="email" id="email" onChange={(e)=>setEmail(e.target.value)} value={email} aria-describedby="emailHelp" placeholder="EMAIL"/>
     	<p></p>
     	<button type="button" onClick={(e)=>{
-                    setError(!emailValid ? 'Invalid Email': '')
+                    handleClick();
+                    setError(!emailValid ? 'Invalid Email': '');
                   } } className="mx-auto d-block btn auth-btns">RESET</button>
     	  	    	</div>
     </div> 
   	)
 }
 
-export default Reset;
+const ResetForm = compose(
+  withFirebase,
+  withRouter)(Reset);
+
+export default ResetForm;
