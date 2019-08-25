2247023601c8b71f4d25aa8193a1449efb99c121
syntaxsizer <qusai.alfaki@gmail.com>
2019-08-25 15:04:04 +0300

    add firebase api

---
+++ src/api/index.js
@@ -0,0 +1,6 @@
+ 
+import FirebaseContext from './context.js';
+import { withFirebase } from './context.js';
+import Firebase from './firebase.js';
+
+export { withFirebase, Firebase, FirebaseContext };
