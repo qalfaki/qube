985b43ac175a50247cd51d3ddb108be88074bd0a
syntaxsizer <qusai.alfaki@gmail.com>
2019-08-25 19:09:37 +0300

    intercept routes and redirect to relevant pages

--- src/App.js
+++ src/App.js
@@ -2,23 +2,27 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
+  Redirect
 } from 'react-router-dom';
 import routes from './routes';
 import './App.css';
 
-function App() {
+const App = (props) => {
+  const [user, setUser] = React.useState(null);
   return (
     <Router>
     	<div className="App">
-     		{routes.map((route,idx)=> <Route key={idx} {...route} />)}
-			<footer>
-      			<small className="footer">
-    				copyrights reserved @Qube
-      			</small>
-    		</footer>
+     	  {routes.map((route, idx)=> <Route key={idx}  path={route.path} render={(props)=> <route.component {...props} currentUser={user} stateHandler = {setUser}/>} />)}
+        <Route path='/'>
+          <Redirect to="/home" />
+        </Route>
+        {user ? null: <Redirect path='/login' to='/login'/>}
+			 <footer>
+      	<small className="footer">copyrights reveresed @Qube</small>
+    	</footer>
     	</div>
     </Router>
-  );
+  )
 }
 
 export default App;
