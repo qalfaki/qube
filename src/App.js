c350ad90f4ba2834cd5e2dbb4d3989785f038cbe
syntaxsizer <qusai.alfaki@gmail.com>
2019-09-07 14:57:40 +0300

    add style to scroll bar and fix bug in tgggle animations

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Sidebar from './components/sidebar.js';
+import Avatar from './components/avatar.js';
+import logo from './assets/images/logo196x143.png';
 import {
   BrowserRouter as Router,
   Route,
@@ -18,22 +21,63 @@ const App = (props) => {
   //  the redirection acts a global intercepter
   //  and will load the relvant page based on the state
   const {user, setUser} = useStorage();
-  const [isLoading, setIsloading] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const [showMenu, setShowMenu] = React.useState(false);
+  const [showSideBar, setShowSideBar] = React.useState(false);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    props.firebase.signOut().then(()=>{
+      props.stateHandler(null)
+    })
+  }
 
   return (
     <Router>
     	<div className="App">
         {isLoading ? <span className='overlay'>
         <div className='spinner'><i></i></div></span>: null}
-     	  {routes.map((route, idx)=> <Route key={idx}  path={route.path} render={(props)=> <route.component {...props} setIsloading={setIsloading} currentUser={user} stateHandler = {setUser}/>} />)}
-        <Route path='/'>
-          <Redirect to="/home" />
-        </Route>
-        {user ? null: <Redirect path='/login' to='/login'/>}
-			 <footer>
-      	<small className="footer">copyrights reveresed @Qube</small>
-    	</footer>
-    	</div>
+
+
+<nav className="nav-bar fixed-top row co-12">
+    <div className="col-4">
+    <button type="button" onClick={(e)=>setShowSideBar(!showSideBar)} className={showSideBar ? "menu-btn_active btn btn-dark btn-circle btn-xl": "btn btn-dark btn-circle btn-xl"}><span></span></button>
+    </div>
+    <div className="col-4">
+    <a href="/home">
+      <img  src={logo} alt='no img' className="logo mx-auto d-block"/>
+    </a>
+    </div>
+    <div className="col-4">
+    <span onClick={()=>setShowMenu(!showMenu)}>
+
+      <Avatar imgSrc={props.currentUser ? props.currentUser.photoURL: null}/>
+    </span>
+    </div>
+  </nav>
+  <div className="wrapper">
+      <Sidebar toggle={showSideBar}/>
+      <div className={showSideBar ? "page-content toggle-page-content": "page-content untoggle-page-content"}>
+        {
+          showMenu
+            ? (<div className="avatar-menu">
+          <a className="dropdown-item" href="#" onClick={
+            handleClick
+          }>logout</a>
+              </div>): (null)
+        }
+     	  {routes.map((route, idx)=> <Route key={idx}  path={route.path} render={(props)=> <route.component {...props} setIsLoading={setIsLoading} currentUser={user} stateHandler = {setUser}/>} />)}
+           <Route path='/'>
+            <Redirect to="/home" />
+          </Route>
+          {user ? null: <Redirect path='/login' to='/login'/>}
+    	  </div>
+      </div>
+      <footer>
+        <small className="footer">copyrights reveresed @Qube</small>
+      </footer>
+      </div>
     </Router>
   )
 }
--- src/components/home.js
+++ src/components/home.js
@@ -16,44 +16,15 @@ const Home = (props) =>{
 	  })
 	}
 
-  	return (<div>
-	<nav className="nav-bar row co-12">
-		<div className="col-4">
-		<button type="button" onClick={(e)=>setShowSideBar(!showSideBar)} className={showSideBar ? "menu-btn_active btn btn-dark btn-circle btn-xl": "btn btn-dark btn-circle btn-xl"}><span></span></button>
-		</div>
-		<div className="col-4">
-		<a href="/home">
-			<img  src={logo} alt='no img' className="logo mx-auto d-block"/>
-		</a>
-		</div>
-		<div className="col-4">
-		<span onClick={()=>setShowMenu(!showMenu)}>
-
-			<Avatar imgSrc={props.currentUser ? props.currentUser.photoURL: null}/>
-		</span>
-		</div>
-	</nav>
-  		<div className="wrapper">
-
-		<Sidebar toggle={showSideBar}/>
-		<div className={showSideBar ? "page-content toggle-page-content": "page-content"}>
-		 {
-          showMenu
-            ? (<div className="avatar-menu">
-    			<a className="dropdown-item" href="#" onClick={
-    				handleClick
-    			}>logout</a>
-              </div>): (null)
-        	}
+  return (
+  	<div>
 			<div className="cheat-sheet-container">
-			<h5>CHEAT SHEET</h5>
-			<div>SAVE: <b>Ctl + s</b></div>
-			<div>UNSAVE: <b>Esc</b></div>
-		</div>
-		<Todo name={props.currentUser && props.currentUser.name ? props.currentUser.name.split(' ')[0]: ''}/>
-		</div>
-  	</div>
-  </div>)
+				<h5>CHEAT SHEET</h5>
+				<div>SAVE: <b>Ctl + s</b></div>
+				<div>CANCEL: <b>Esc</b></div>
+			</div>
+			<Todo {...props} setIsLoading={props.setIsLoading} name={props.currentUser && props.currentUser.name ? props.currentUser.name.split(' ')[0]: ''}/>
+		</div>)
 }
 
 export default withFirebase(Home)
