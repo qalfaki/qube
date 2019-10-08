63bad61d8e912a41fde16b16c3f4bb2ceaae8f41
syntaxsizer <qusai.alfaki@gmail.com>
2019-10-08 21:13:18 +0300

    fix responsiveness issues in login form

--- src/components/login.js
+++ src/components/login.js
@@ -22,20 +22,21 @@ const Login = (props) =>{
     }
   }
   return (
-  	<div className="top-height col-12 row"><div className="mx-auto d-block">
-    	  	    <img src={logo} alt='no img' className="mx-auto d-block"/>
-              <p className="error-text">{error}</p>
-    	  	    <input className="input-field form-control d-block mx-auto" type="email" id="email" onChange={(e)=>{setError(''); setEmail(e.target.value)}} value={email} aria-describedby="emailHelp" placeholder="EMAIL"/>
-    	  	    <input className="input-field form-control d-block mx-auto" type="password" id="password" onChange={(e)=>{setPassword(e.target.value); setError('')}} value={password} aria-describedby="emailHelp" placeholder="PASSWORD"/>
-    	  	    <div className="btn-wrapper">
-    	  	    <Link to="/signup" className="link">SIGNUP</Link> <span className="error-text">|</span> <Link to="/reset" className="link">RESET PASSWORD</Link>
-    	  	    		<button type="button" onClick={(e)=>{
-                    setError(!emailValid && !passwordValid ? 'Invalid Credentials': !emailValid ? 'Invalid Email': !passwordValid ? 'password must be at least 6 characters':'');
-                    handleClick()
-                  } } className="btn float-right auth-btns">LOGIN</button>
-    	  	    	</div>
-    	  	    </div>
-    	  	</div>
+  	<div className="grid login">
+      <div className="item column-wrapper">
+    	  <img src={logo} alt='no img' className=""/>
+        <p className="item error-text">{error}</p>
+    	  <input className="item input-field form-control" type="email" id="email" onChange={(e)=>{setError(''); setEmail(e.target.value)}} value={email} aria-describedby="emailHelp" placeholder="EMAIL"/>
+    	  <input className="item input-field form-control" type="password" id="password" onChange={(e)=>{setPassword(e.target.value); setError('')}} value={password} aria-describedby="emailHelp" placeholder="PASSWORD"/>
+    	  <div className="btn-wrapper">
+    	    <Link to="/signup" className="link">SIGNUP</Link> <span className="error-text">|</span> <Link to="/reset" className="link">RESET PASSWORD</Link>
+    	    <button type="button" onClick={(e)=>{
+            setError(!emailValid && !passwordValid ? 'Invalid Credentials': !emailValid ? 'Invalid Email': !passwordValid ? 'password must be at least 6 characters':'');
+            handleClick()
+          }} className="btn auth-btns">LOGIN</button>
+    	 </div>
+      </div>
+    </div>
   	)
 }
 
